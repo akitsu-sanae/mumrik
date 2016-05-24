e2f238823753f26a341b51aea0f49a9b07a5ff5a
akitsu-sanae <akitsu.sanae@gmail.com>
2016-05-24 20:00:47 +0900

    dependent type

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,8 @@ pub enum Expression {
 pub enum Type {
     Primary(String),
     Union(Box<Type>, Box<Type>),
-    Tuple(Box<Type>, Box<Type>)
+    Tuple(Box<Type>, Box<Type>),
+    Dependent(String, Box<Type>)
 }
 
 use syntax::*;
@@ -97,24 +98,71 @@ fn expression_test() {
                 box Expression::NumberLiteral(23),
                 )));
 
+    assert_eq!(expression("fib@(n+-1) + fib@(n+-2)"), Ok(
+            Expression::Add(
+                box Expression::Apply(
+                    box Expression::Identifier("fib".to_string()),
+                    box Expression::Add(
+                        box Expression::Identifier("n".to_string()),
+                        box Expression::NumberLiteral(-1)
+                        )
+                    ),
+                box Expression::Apply(
+                    box Expression::Identifier("fib".to_string()),
+                    box Expression::Add(
+                        box Expression::Identifier("n".to_string()),
+                        box Expression::NumberLiteral(-2)
+                        )
+                    )
+            )));
 
 
 }
 
 #[test]
 fn function_test() {
-    assert_eq!(function("func main arg: Int { 0 }"), Ok(
+    assert_eq!(function("func main args: List[String] { 0 }"), Ok(
         Function{
             name: "main".to_string(),
-            arg_name: "arg".to_string(),
-            arg_type: box Type::Primary("Int".to_string()),
+            arg_name: "args".to_string(),
+            arg_type: box Type::Dependent(
+                "List".to_string(),
+                box Type::Primary("String".to_string())),
             body: box Expression::NumberLiteral(0)
         }));
-    assert_eq!(function("func main arg: Int { std.io.println@123 }"), Ok(
+    assert_eq!(function("
+                        func fib n: Int {
+                            fib@(n-1) + fib@(n-2)
+                        }"), Ok(
+        Function{
+            name: "fib".to_string(),
+            arg_name: "n".to_string(),
+            arg_type: box Type::Primary("Int".to_string()),
+            body: box Expression::Add(
+                box Expression::Apply(
+                    box Expression::Identifier("fib".to_string()),
+                    box Expression::Sub(
+                        box Expression::Identifier("n".to_string()),
+                        box Expression::NumberLiteral(1)
+                        )
+                    ),
+                box Expression::Apply(
+                    box Expression::Identifier("fib".to_string()),
+                    box Expression::Sub(
+                        box Expression::Identifier("n".to_string()),
+                        box Expression::NumberLiteral(2)
+                        )
+                    )
+                )
+        }));
+    assert_eq!(function("
+                        func main args: List[String * Int] {
+                            std.io.println@123
+                        }"), Ok(
         Function{
             name: "main".to_string(),
-            arg_name: "arg".to_string(),
-            arg_type: box Type::Primary("Int".to_string()),
+            arg_name: "args".to_string(),
+            arg_type: box Type::Dependent("List".to_string(), box Type::Tuple(box Type::Primary("String".to_string()), box Type::Primary("Int".to_string()))),
             body: box Expression::Apply(
                 box Expression::Dot(
                     box Expression::Identifier("std".to_string()),
