f71653ce153b12c4a0de5099d87a8a5b981c1313
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-13 00:37:23 +0900

    fix LICENSE

--- README.md
+++ README.md
@@ -30,3 +30,10 @@ func main args: List[String] {
 }
 ```
 
+# Copyright
+Copyright (C) 2016 akitsu sanae.  
+Distributed under the Boost Software License, Version 1.0. 
+(See accompanying file LICENSE or copy at http://www.boost/org/LICENSE_1_0.txt)  
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*============================================================================
+  Copyright (C) 2015-2016 akitsu sanae
+  https://github.com/akitsu-sanae/mumrik
+  Distributed under the Boost Software License, Version 1.0. (See accompanying
+  file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+============================================================================*/
+
 #![feature(plugin)]
 #![plugin(peg_syntax_ext)]
 
