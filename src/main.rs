3d2aab0ecb66f9139bbe53913b6dcd4b5425f63a
nanjo <akitsu.sanae@gmail.com>
2016-09-06 15:02:47 +0900

    repl mode

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 #![feature(box_patterns)]
 
 use std::io;
+use std::io::Write;
+use std::io::stdout;
 
 #[macro_use]
 extern crate nom;
@@ -10,31 +12,27 @@ extern crate nom;
 mod ast;
 mod parser;
 mod eval;
+mod test;
 
 use parser::expression;
 
 fn main() {
-    println!("{:?}", parser::expression(b"1"));
-    println!("{:?}", parser::expression(b"1+2"));
-    println!("{:?}", parser::expression(b"1*2"));
-    println!("{:?}", parser::expression(b"4+1*2"));
-    println!("{:?}", parser::expression(b"5*4+1-4"));
-    println!("{:?}", parser::expression(b"hoge"));
-    println!("{:?}", parser::expression(b"hoge+1"));
-    println!("{:?}", parser::expression(b"let a = 1+2; 12"));
-    println!("{:?}", parser::expression(b"let a = 1+2; let b = 2+5; a*b"));
-    println!("{:?}", eval::eval(parser::expression(b"123"), &vec![]));
-    println!("{:?}", eval::eval(parser::expression(b"123+1"), &vec![]));
-    println!("{:?}", eval::eval(parser::expression(b"123*2"), &vec![]));
-    println!("{:?}", eval::eval(parser::expression(b"let a = 1+2; 12"), &vec![]));
-    println!("{:?}", eval::eval(parser::expression(b"let b = 4*3; b+5"), &vec![]));
-    println!("{:?}", eval::eval(parser::expression(b"let a = 4*3; let b = 3; a+b"), &vec![]));
     loop {
+        print!("# ");
+        stdout().flush().unwrap();
         let mut line = String::new();
         io::stdin().read_line(&mut line).unwrap();
-        let expr = expression(line.as_bytes());
-        println!("{:?}", expr);
-        println!("{:?}", eval::eval(expr, &vec![]));
+
+        match line.as_str().trim() {
+            "quit" => return,
+            "help" => {
+                println!("how to use")
+            },
+            _ => {
+                let expr = expression(line.as_bytes());
+                println!(" => {:?}", eval::eval(expr, &vec![]));
+            },
+        }
     }
 }
 
