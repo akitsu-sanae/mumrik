accee672a83adec53e4c24cee05bb9f56a196994
akitsu-sanae <akitsu.sanae@gmail.com>
2020-04-01 23:59:58 +0900

    udpate

--- src/typecheck.rs
+++ src/typecheck.rs
@@ -46,8 +46,8 @@ pub fn check_func(func: &parsed::Func) -> Result<(Ident, typed::Expr, typed::Typ
 
 pub fn check_expr(e: &parsed::Expr, env: &Env<typed::Type>) -> Result<typed::Expr, Error> {
     match e {
-        parsed::Expr::Const(lit) => Ok(typed::Expr::Const(check_lit(lit, env)?)),
-        parsed::Expr::Var(name, pos) => Ok(typed::Expr::Var(
+        parsed::Expr::Const(ref lit) => Ok(typed::Expr::Const(check_lit(lit, env)?)),
+        parsed::Expr::Var(ref name, ref pos) => Ok(typed::Expr::Var(
             name.clone(),
             env.lookup(name).map_err(|_| {
                 Error::UnboundVariable(UnboundVariableError {
@@ -56,7 +56,7 @@ pub fn check_expr(e: &parsed::Expr, env: &Env<typed::Type>) -> Result<typed::Exp
                 })
             })?,
         )),
-        parsed::Expr::Lambda(param_name, param_type, box body, _) => {
+        parsed::Expr::Lambda(ref param_name, ref param_type, box ref body, _) => {
             let param_type = typed::Type::from_parsed_type(param_type);
             let env = env.add(param_name.clone(), param_type.clone());
             let body = check_expr(body, &env)?;
@@ -69,9 +69,30 @@ pub fn check_expr(e: &parsed::Expr, env: &Env<typed::Type>) -> Result<typed::Exp
         parsed::Expr::Apply(box ref e1, box ref e2) => {
             let e1 = check_expr(e1, env)?;
             let e2 = check_expr(e2, env)?;
-            Ok(typed::Expr::Apply(box e1, box e2))
+            let t1 = typed::type_of(&e1);
+            if let typed::Type::Func(box param_ty, _) = t1 {
+                let t2 = typed::type_of(&e2);
+                if t2 == param_ty {
+                    Ok(typed::Expr::Apply(box e1, box e2))
+                } else {
+                    todo!()
+                    /*
+                    Err(Error::UnmatchType(UnmatchTypeError {
+                        pos: pos,
+                        expected: param_ty,
+                        actual: t2,
+                    })) */
+                }
+            } else {
+                todo!()
+                /* Err(Error::UnmatchType(UnmatchTypeError {
+                    pos: Position { start: 0, end: 0 }, // TODO
+                    expected: typed::Type::Func(box typed::Type::Int, box typed::Type::Int), // TODO,
+                    actual: t1,
+                })) */
+            }
         }
-        parsed::Expr::Let(name, box ref e1, box ref e2, _) => {
+        parsed::Expr::Let(ref name, box ref e1, box ref e2, _) => {
             let e1 = check_expr(e1, env)?;
             let env = env.add(name.clone(), typed::type_of(&e1));
             let e2 = check_expr(e2, &env)?;
@@ -81,16 +102,60 @@ pub fn check_expr(e: &parsed::Expr, env: &Env<typed::Type>) -> Result<typed::Exp
             let e = e.subst_type(name, typ);
             check_expr(&e, env)
         }
-        parsed::Expr::If(box cond, box e1, box e2, _) => Ok(typed::Expr::If(
+        parsed::Expr::If(box ref cond, box ref e1, box ref e2, _) => Ok(typed::Expr::If(
             box check_expr(cond, env)?,
             box check_expr(e1, env)?,
             box check_expr(e2, env)?,
         )),
-        parsed::Expr::BinOp(op, box e1, box e2) => Ok(typed::Expr::BinOp(
+        parsed::Expr::BinOp(ref op, box ref e1, box ref e2) => Ok(typed::Expr::BinOp(
             *op,
             box check_expr(e1, env)?,
             box check_expr(e2, env)?,
         )),
+        parsed::Expr::Sequence(ref es) => {
+            if let &[ref es @ .., ref e] = es.as_slice() {
+                es.iter().fold(Ok(check_expr(&e, env)?), |acc, e| {
+                    if let Ok(acc) = acc {
+                        let e = check_expr(&e, env)?;
+                        Ok(typed::Expr::Let(Ident::new("<dummy>"), box acc, box e))
+                    } else {
+                        acc
+                    }
+                })
+            } else {
+                unreachable!()
+            }
+        }
+        parsed::Expr::FieldAccess(box ref e, ref label_, _) => Ok(typed::Expr::FieldAccess(
+            box check_expr(e, env)?,
+            label_.clone(),
+        )),
+        parsed::Expr::PatternMatch(box ref e, ref arms, _) => {
+            let e = check_expr(e, env)?;
+            let fields_ty = if let typed::Type::Record(fields_ty) = typed::type_of(&e) {
+                fields_ty
+            } else {
+                unreachable!() // TODO
+            };
+            let arms: Result<_, _> = arms
+                .iter()
+                .map(|(ref arm, _)| {
+                    let field = fields_ty
+                        .iter()
+                        .find(|(ref label, _)| &arm.label == label)
+                        .unwrap();
+                    let env = env.add(arm.name.clone(), field.1.clone());
+                    let body = check_expr(&arm.body, &env)?;
+                    Ok(typed::PatternMatchArm {
+                        label: arm.label.clone(),
+                        name: arm.name.clone(),
+                        body: body,
+                    })
+                })
+                .collect();
+            Ok(typed::Expr::PatternMatch(box e, arms?))
+        }
+        parsed::Expr::Println(box ref e, _) => Ok(typed::Expr::Println(box check_expr(e, env)?)),
         _ => todo!(),
     }
 }
